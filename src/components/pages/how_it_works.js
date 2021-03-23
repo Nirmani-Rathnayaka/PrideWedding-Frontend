4d2a4fd903aed51306e547013fb5a2dc68cd6f37
Nirmani-Rathnayaka <nirmaninipunika053@gmail.com>
2021-03-24 01:09:50 +0530

    Footer components

---
+++ src/components/pages/how_it_works.js
@@ -0,0 +1,18 @@
+
+import { Link } from 'react-router-dom';
+
+import React,{Component} from 'react';
+
+export default class How_it_works extends Component {
+render() {
+    return (
+       
+		<form>
+           
+
+		</form>
+
+        );
+    }
+}
+
