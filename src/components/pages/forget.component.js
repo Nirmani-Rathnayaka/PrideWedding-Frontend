8efb979ec6bf294f6d0356381becb003817fdcd4
Nirmani Rathnayaka <68730497+Nirmani-Rathnayaka@users.noreply.github.com>
2021-06-09 23:27:51 +0530

    Update forget.component.js

--- src/components/pages/forget.component.js
+++ src/components/pages/forget.component.js
@@ -1,22 +1,30 @@
 import React, { Component } from "react";
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import "./index.css";
 export default class Forget extends Component {
+
+onSubmit=()=>{
+    alert("You will recieve Password reset Email shortly")
+}
 
     render() {
         return (
          
             <form>
                 <br></br>
-                <h3>Forgot Password</h3>
+                <h3 className="testClass">Forgot Password</h3>
 
                 <div className="form-group">
                     <label>Email</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control"
+                     placeholder="Enter email"
+                     required 
+                    />
                 </div>
-                  <button type="submit" className="btn btn-dark btn-lg btn-block">Submit</button>
-                
+                <Link to= {"/"}> 
+                  <button type="submit" onClick={this.onSubmit} className="btn btn-dark btn-lg btn-block">Submit</button>
+                  </Link>
             </form>
           
         );
